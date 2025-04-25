31ed9af18435d72b59649e748df01a9811b57019
vudinhso <vudinhso@gmail.com>
2025-04-25 10:23:53 +0700

    cleaner code

--- src/rmitbot_description/launch/display.launch.py
+++ src/rmitbot_description/launch/display.launch.py
@@ -1,7 +1,7 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-import os
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command, LaunchConfiguration
@@ -48,7 +48,6 @@ def generate_launch_description():
         output='screen',
         arguments=['-d', os.path.join(rmitbot_description_dir, 'rviz', 'display.rviz')],
     )
-    
     
     return LaunchDescription([
         model_arg, 
